25cd521855c3045fa98aaf46fded6d9c6bdea550
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-14 16:02:15 +0100

    Genero i post da JS

--- main.js
+++ main.js
@@ -55,3 +55,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// recupero il container dei post
+post = document.getElementById("container");
+
+posts.forEach((item, index, array) => {
+
+    let contenuto = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${item.author.image} alt=${item.author.name}>                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${item.author.name}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${item.content}</div>
+            <div class="post__image">
+                <img src=${item.media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${item.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+    ;
+
+    post.innerHTML += contenuto;
+});
